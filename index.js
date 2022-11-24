e4f9bd47a45efeb650b56d055071bdb797f3f111
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-11-24 22:35:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const receivesAFunction = (callback) => {
+  callback();
+};
+const returnsANamedFunction = () => {
+  return function namedFunction() {};
+};
+const returnsAnAnonymousFunction = () => {
+  return function () {};
+};
